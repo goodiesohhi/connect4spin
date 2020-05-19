8a60a3780f8a576af9062676d5990cc06cec49af
Alex Hong <goodiesohhi@hotmail.com>
2020-05-19 02:21:59 -0400

    qg update

--- games/qg/first.js
+++ games/qg/first.js
@@ -1,20 +1,35 @@
 room={}
-room.name="Hall of Beginnings"
+room.name="Cave of Beginnings (Pronounced Beh-gennings (stressed first syllable))"
 room.getTXT={
 
-  "a;1": "Hello testing",
-  "a;2": "Hello testing2",
+  "a;1": "You awake on the cold damp floor. You're in a cave. ",
+  "a;2": "Yes I know it's cliche but it's 2am right now for me just go along with it",
+  "a;3": "You awake in a cave. ",
+  "a;4":"You take a look through the hole. It's very dark and you can't see anything at all!"
 
 }
 room.getIMG={
-"a;1":"askiosk",
+"a;1":"cave1.png",
+"a;2":"2am.png",
+"a;3":"cave2.png",
+"a;4":"hole.png"
 }
 room.getCHOICE={
-  "a;1":[[  "Proceed forward", "n"]],
+  "a;1":[[  "Take note of the cliche", "n"]],
 
 
-  "a;2":[[  "Turn towards Rat City", "ratcity.js"]]
+  "a;2":[[  "Go along with it", "n"]],
+  "a;3":[["The hole","n"]],
 
+
+
+}
+
+room.isMap={
+
+  "a;3": `<map id="map" name="map">
+    <area target="" id="0" class="mclick" alt="Hole" title="Hole" href="#" coords="694,204,780,174,883,226,883,348,826,428,746,317" shape="poly">
+  </map>`
 
 }
 //room.
