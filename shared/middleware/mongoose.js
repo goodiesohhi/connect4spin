178e74215768b36aaca978f4beb8049743dd244f
Alex Hong <goodiesohhi@hotmail.com>
2020-12-06 16:00:25 -0500

    s

--- shared/middleware/mongoose.js
+++ shared/middleware/mongoose.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 module.exports = function() {
 
-  var MONGODB_URI = "mongodb://localhost:3000/test";
+  var MONGODB_URI = "mongodb://localhost:27017/";
 
   mongoose.Promise = global.Promise;
 
-  var yes=mongoose.connect(MONGODB_URI);
+  var yes=mongoose.connect( MONGODB_URI, {useNewUrlParser: true});
 
   return yes;
 }
